5acc381ddd4371c277de19e288393cd72bac5aea
JoeyCMacBook <coderjoe05@icloud.com>
2019-12-21 11:38:59 -0500

    Start porting master to GovPotential
    With some improvements

--- events/message.js
+++ events/message.js
@@ -1,62 +1,19 @@
 module.exports = (client, db, msg) => {
   let path = `/Users/Kids/Documents/GitHub/dbt-beta/`
 
-  const multiSearch = require(`${path}libs/multiSearch.js`)
+  const multiSearch = require(`./libs/multiSearch.js`)
+  const removeFirstMention = require(`./libs/multiSearch.js`)
 
   var d = new Date()
 
   if (msg.author.bot) return
 
-  const key = `${msg.guild.id}-${msg.author.id}`
+  var msgNoMention = removeFirstMention(msg)
 
-  /*
-  --Data Table--
-  client.data.ensure(key, {
-  user: msg.author.id,
-  guild: msg.guild.id,
-  points: 0,
-  level: 1,
-  lastPointMsg: 0,
-  warntimes: 0
-  });
-  */
-
-  const stmt = db.prepare(`SELECT * FROM users WHERE key = ${key};`)
-  let userData = stmt.get()
-
-  let badWords = [`fuck`, `shit`, `ass`, `bitch`, `nigger`]
-
-  if (multiSearch.multiSearchFor(msg, badWords)) {
-    msg.delete()
-      .then(msg => console.log(`Deleted message from ${msg.author.username}, due to LANGUAGE`))
-      .catch(console.error)
-    msg.reply(`Why so salty? No bad language in ${msg.guild}`)
-    let sql = db.prepare(`UPDATE users SET warnTimes = ${userData.warntimes + 1} WHERE key = ${key};`)
-    sql.run()
-    const channel = msg.guild.channels.find(ch => ch.name === 'logs')
-    if (channel) {
-      channel.send(`warned user: ${msg.author.username} due to LANGUAGE. New warnTimes value: ${userData.warntimes + 1}`)
-    }
-  }
-
-  let timeSent = d.getTime()
-
-  if (userData.lastPointMsg < (timeSent - 60000)) {
-    let sql = db.prepare(`UPDATE users SET points = ${userData.points + 1} WHERE key = ${key}`)
-    sql.run()
-    sql = db.prepare(`UPDATE users SET lastPointMsg = ${timeSent} WHERE key = ${key}`)
-    sql.run()
-  }
-
-  const curLevel = Math.floor(
-    0.25 * Math.sqrt(userData.points)
-  )
-
-  console.log(`User ${msg.author.username} now has ${userData.points} points. Level ${curLevel} expected, level ${userData.level}, lastSent = ${userData.lastPointMsg}`)
-
-  if (userData.level < curLevel) {
-    msg.reply(`You've leveled up to level **${curLevel}**!`)
-    let sql = db.prepare(`UPDATE users SET level = ${curLevel} WHERE key = ${key};`)
-    sql.run()
+  if (msgNoMention == 'GP is gay?') {
+    var resp = isGayResponses[Math.floor(Math.random() * isGayResponses.length)];
+    
   }
 }
+
+var isGayResponses = [`So gay, the rainbows are jealous.`,`He is taken, don't bother.`,`Are you asking for a friend?`]
--- events/ready.js
+++ events/ready.js
@@ -1,35 +1,7 @@
 module.exports = (client, db, msg) => {
-  function addDefaultData (UId, UGuild) {
-    let key = `${UGuild}-${UId}`
-    let sql = db.prepare(`UPDATE users SET user = "${UId}", guild = ${UGuild}, points = 0, level = 0, lastPointMsg = 0, warntimes = 0 WHERE key = ${key};`)
-    sql.run()
-  }
 
   console.log(`Logged in as ${client.user.tag}!`)
 
-  var guilds = Array.from(client.guilds.values())
-  console.log(guilds.length)
-  console.log(`running db check`)
-
-  for (var j = 0; j < guilds.length; j++) {
-    console.log(j)
-    let guild = guilds[j]
-    let guildMembers = Array.from(guild.members.values())
-
-    for (var i = 0; i < guildMembers.length; i++) {
-      console.log(i)
-      let guildUser = guildMembers[i]
-      if (guildUser.user.bot) return
-      var key = `${guild.id}-${guildUser.user.id}`
-      let stmt = db.prepare(`INSERT OR IGNORE INTO users (key) SELECT (${key}) WHERE NOT EXISTS (SELECT 1 FROM users WHERE key = "${key}");`)
-      let changes = stmt.run()
-
-      if (changes.changes === 1) {
-        console.log(`A new user has been inserted with key ${key}`)
-        addDefaultData(guildUser.user.id, guild.id)
-      }
-    }
-  }
 }
 // db.run(`INSERT INTO users(key) VALUES(${guildUser.id}, ${guild.id}, 0, 0, 0, 0) WHERE NOT EXISTS(SELECT 1 FROM users WHERE key="${key}")`, [key]
 // console.log(`user ${guildUser.id} is already in the db. Skipping...`);
