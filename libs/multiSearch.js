5c357094b25395a8e7175ee5002d95ba2488ba3e
JoeyCMacBook <coderjoe05@icloud.com>
2019-12-21 18:19:27 -0500

    Generic Improvements

--- libs/multiSearch.js
+++ libs/multiSearch.js
@@ -1,6 +1,4 @@
-module.exports = {
-  multiSearchFor: function (text, searchWords) {
-    var searchExp = new RegExp(searchWords.join(`|`), `gi`)
-    return (searchExp.test(text))
-  }
+export function multiSearchFor(text, searchWords) {
+  var searchExp = new RegExp(searchWords.join(`|`), `gi`);
+    return (searchExp.test(text));
 }
---
+++ libs/removeFirstMention.js
@@ -0,0 +1,12 @@
+export function removeFirstMention(str) {
+  var n = str.search("@"); //Search for @
+  firstElement = str.slice(0,n); //Remove first part of the String, this part is finished
+  secondElementUnfinished = str.slice(n, str.length); //remove second part of string, this part needs more handling
+  for (var i = 0; i < secondElementUnfinished.length; i++) { //Iterate through the string
+  	if (secondElementUnfinished.charAt(i) === " ") { //Look for a space
+    	var h = i //Show space location
+    }
+  }
+  secondElement = secondElementUnfinished.slice(h,secondElementUnfinished.length); //Extract area from space
+  return firstElement + secondElement
+}
