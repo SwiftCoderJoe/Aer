339b7c97aac8da294a5a64790535366d36211fa6
JoeyCMacBook <coderjoe05@icloud.com>
2019-12-21 18:52:28 -0500

    Add some jokes

--- events/message.js
+++ events/message.js
@@ -12,13 +12,20 @@ module.exports = (client, db, msg) => {
   if (msg.mentions.users.first()) {
     var msgNoMention = removeFirstMention(msg)
 
-    switch (msgNoMention) {
-      case 'GP is gay?': {
+    switch (msgNoMention.toLowerCase()) {
+      case 'gp is gay?': {
         var resp = isGayResponses[Math.floor(Math.random() * isGayResponses.length)];
         msg.reply(resp)
       }
     }
+  } else {
+    if (msg.toLowerCase() == `gp tell me a joke`) {
+      var resp = jokes[Math.floor(Math.random() * jokes.length)];
+      msg.reply(resp)
+    }
   }
+
 }
 
 var isGayResponses = [`So gay, the rainbows are jealous.`,`He is taken, don't bother.`,`Are you asking for a friend?`]
+var jokes = [`What's red and bad for your teeth? A brick.`, `I was going to tell a dead baby joke. But I decided to abort.`]
