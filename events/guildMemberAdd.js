792697c87bf9529cc1fa5bf5740e70b515bdb0d1
JoeyCMacBook <coderjoe05@icloud.com>
2019-12-21 18:19:50 -0500

    Remove commands and comments

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 module.exports = (client, db, member) => {
-  let greetingsFirst = [`Greetings, `, `Whooosh, `, `Welcome to the server, `]
-  let greetingsAfter = [` we are glad to have you.`, ` has landed.`, ``]
+  let greetingsFirst = [`Greetings, `, `Welcome to the server, `]
+  let greetingsAfter = [` we are glad to have you.`, ``]
   const channel = member.guild.channels.find(ch => ch.name === 'new-members')
   if (!channel) return
   let greetingNum = Math.floor((Math.random() * greetingsFirst.length) + 1)
