d62a4d65dca444bb303ba9b4d698225694d1ef1d
SwiftCoderJoe <coderjoe05@gmail.com>
2019-12-22 18:00:03 -0500

    Update with special message checks

--- events/message.js
+++ events/message.js
@@ -21,10 +21,22 @@ module.exports = (client, db, msg) => {
       }
     }
   } else {
-    if (msg.content.toLowerCase() == `gp tell me a joke`) {
-      console.log(`joke`);
-      var resp = jokes[Math.floor(Math.random() * jokes.length)];
+
+
+
+    if (msg.content.startsWith(`yes or no`)) {
+      var resp = yesno[Math.floor(Math.random() * yesno.length)];
       msg.reply(resp)
+    } else {
+
+
+
+      switch (msg.content.toLowerCase()) {
+        case `GP tell me a joke`: {
+          var resp = jokes[Math.floor(Math.random() * jokes.length)];
+          msg.reply(resp)
+        }
+      }
     }
   }
 
@@ -38,3 +50,4 @@ var jokes = [`What's red and bad for your teeth? A brick.`,
   `Why can't orphans play basketball? They don't know where home is.`,
   `Give a man a match, and he'll be warm for a few hours. Set a man on fire, and he will be warm for the rest of his life.`,
   `I asked a pretty, young homeless woman if I could take her home. She smiled at me and said yes. The look on her face soon changed, however, when I walked off with her cardboard box.`]
+  var yesno = [`yes`,`no`]
