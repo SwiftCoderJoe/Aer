d40721c11bc0c0eb115a5f35ab2fb7f938b58014
Joe Linux Environment <coderjoe05@gmail.com>
2020-11-02 16:27:52 -0500

    Add removeWarn and setWarns

---
+++ commands/moderation/removeWarn.js
@@ -0,0 +1,77 @@
+const { Command } = require(`discord.js-commando`)
+
+module.exports = class warnsCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: `removeWarn`,
+      aliases: [],
+      group: `moderation`,
+      memberName: `removeWarn`,
+      description: `Removes a warn for a specific user.`,
+      guildOnly: true,
+      examples: [`removeWarn @user#0000`],
+      args: [
+        {
+          key: `user`,
+          prompt: `Who would you like to remove warn from?`,
+          type: `user`
+        }
+      ]
+    })
+  }
+  run (msg, { user }) {
+    try {
+      // Constants
+      const Database = require(`better-sqlite3`)
+      const config = require(`${process.cwd()}/config/config.json`)
+      var strongRoles = config[msg.guild.id].moderation.modRoles
+      let guildMembers = msg.guild.members 
+      let callMember = guildMembers.cache.get(msg.author.id)
+      let callMemberRoles = Array.from(callMember.roles.cache.array())
+      const key = `g${msg.guild.id}u${user.id}`
+      const db = new Database(`${process.cwd()}/db/Data.db`, { /* verbose: console.log */ })
+
+      // Check if permissions work
+      var canWarn = false
+      strongRoles.some(function (requiredRole, _index1) {
+        for (let role of callMemberRoles) {
+          if (role.id === requiredRole) {
+            canWarn = true
+            return true
+          }
+        }
+        return false
+      })
+
+      
+      if (canWarn) {
+        // Carry out removewarn
+        const stmt = db.prepare(`UPDATE users SET warntimes = warntimes - 1 WHERE key = "${key}";`)
+        stmt.run()
+
+        // Reply
+        msg.say(`User ${user.username} was warned successfully.`)
+        const channel = msg.guild.channels.cache.find(ch => ch.name === 'logs')
+
+        // Log
+        if (channel) {
+          const stmt = db.prepare(`SELECT warntimes FROM users WHERE key = "${key}";`)
+          const newWarns = stmt.get()
+          channel.send(`${msg.author} removed a warn from user: ${user.username}.\nNew warnTimes value: ${newWarns.warntimes}`)
+        }
+
+      } else {
+
+        msg.say(`You cannot warn this user.`)
+        
+      }
+      
+    } catch (e) {
+      msg.reply(
+        `An error has occured. Try waiting for a moment before retrying. Error: (${
+          e
+        })`
+      )
+    }
+  }
+}
---
+++ commands/moderation/setWarns.js
@@ -0,0 +1,83 @@
+const { Command } = require(`discord.js-commando`)
+
+module.exports = class warnsCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: `setWarns`,
+      aliases: [],
+      group: `moderation`,
+      memberName: `setWarns`,
+      description: `Sets the WarnTimes for a specific user.`,
+      guildOnly: true,
+      examples: [`setWarns @user#0000 0`],
+      args: [
+        {
+          key: `user`,
+          prompt: `Who would you like to warn?`,
+          type: `user`
+        },
+        {
+          key: `number`,
+          prompt: `What should the Warns be set to?`,
+          type: `integer` 
+        }
+
+      ]
+    })
+  }
+  run (msg, { user, number }) {
+    try {
+      // Constants
+      const Database = require(`better-sqlite3`)
+      const config = require(`${process.cwd()}/config/config.json`)
+      const strongRoles = config[msg.guild.id].moderation.modRoles
+      const guildMembers = msg.guild.members 
+      const callMember = guildMembers.cache.get(msg.author.id)
+      const callMemberRoles = Array.from(callMember.roles.cache.array())
+      const db = new Database(`${process.cwd()}/db/Data.db`, { /* verbose: console.log */ })
+      const key = `g${msg.guild.id}u${user.id}`
+
+      // Check if permissions work
+      var canWarn = false
+      strongRoles.some(function (requiredRole, _index1) {
+        for (let role of callMemberRoles) {
+          if (role.id === requiredRole) {
+            canWarn = true
+            return true
+          }
+        }
+        return false
+      })
+
+
+      if (canWarn) {
+        // Carry out warn
+        const stmt = db.prepare(`UPDATE users SET warntimes = ${number} WHERE key = "${key}";`)
+        stmt.run()
+
+        // Reply
+        msg.say(`Warns for ${user.username} were successfully changed.`)
+
+        // Log
+        const channel = msg.guild.channels.cache.find(ch => ch.name === 'logs')
+        if (channel) {
+          const stmt = db.prepare(`SELECT warntimes FROM users WHERE key = "${key}";`)
+          const newWarns = stmt.get()
+          channel.send(`${msg.author} changed Warntimes value for user: ${user.username}.\nNew warnTimes value: ${newWarns.warntimes}`)
+        }
+
+      } else {
+
+        msg.say(`You cannot warn this user.`)
+
+      }
+      
+    } catch (e) {
+      msg.reply(
+        `An error has occured. Try waiting for a moment before retrying. Error: (${
+          e
+        })`
+      )
+    }
+  }
+}
