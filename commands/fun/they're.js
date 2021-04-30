7d09d25187313c401524a669731c90e4365cd407
SwiftCoderJoe <coderjoe05@gmail.com>
2021-04-30 18:35:30 -0400

    Create they're.js

---
+++ commands/fun/they're.js
@@ -0,0 +1,29 @@
+const { Command } = require(`discord.js-commando`)
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class youngmanCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: `they're`,
+      aliases: [`t`],
+      group: `fun`,
+      memberName: `they're`,
+      description: `aer they're`,
+      guildOnly: true,
+      examples: [`they're`],
+      args: []
+    })
+  }
+  run (msg) {
+    try {
+      msg.reply("youngman")
+      msg.channel.send("https://tenor.com/view/memes-meme-ironic-explosion-gif-18934104")
+    } catch (e) {
+      msg.reply(
+        `An error has occured. Try waiting for a moment before retrying. Error: (${
+          e
+        })`
+      )
+    }
+  }
+}
