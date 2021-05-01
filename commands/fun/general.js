c604aecad8241b5bf1c422237a66a9571d2539b0
SwiftCoderJoe <coderjoe05@gmail.com>
2021-05-01 13:07:49 -0400

    general command

--- commands/fun/general.js
+++ commands/fun/general.js
@@ -4,19 +4,19 @@ const { MessageEmbed } = require('discord.js')
 module.exports = class youngmanCommand extends Command {
   constructor (client) {
     super(client, {
-      name: `they're`,
+      name: `general`,
       aliases: [],
       group: `fun`,
-      memberName: `they're`,
-      description: `aer they're`,
+      memberName: `general`,
+      description: `aer general`,
       guildOnly: true,
-      examples: [`they're`],
+      examples: [`general`],
       args: []
     })
   }
   run (msg) {
     try {
-        msg.channel.send("https://tenor.com/view/memes-meme-ironic-explosion-gif-18934104")
+        msg.channel.send("https://tenor.com/view/discord-staff-staff-gif-19225864")
     } catch (e) {
       msg.reply(
         `An error has occured. Try waiting for a moment before retrying. Error: (${
