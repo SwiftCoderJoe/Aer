e27359403b2cde101b1e405a3179e6be83d50f74
SwiftCoderJoe <coderjoe05@gmail.com>
2021-05-15 15:49:56 -0400

    Update youngman.js

--- commands/fun/youngman.js
+++ commands/fun/youngman.js
@@ -17,11 +17,13 @@ module.exports = class youngmanCommand extends Command {
   run (msg) {
     try {
       switch(msg.guild.id) {
-        case 702241230225932292: 
+        case "702241230225932292":
+          console.log("youngman")
           msg.reply("youngman")
           msg.channel.send("https://media.discordapp.net/attachments/724009203072761876/758293460255506462/image0_31-1.gif")
           break;
-        case 716011492071440505:
+        case "716011492071440505":
+          console.log("no youngman")
           msg.reply("Anders made me turn this off. No youngman for you.")
           break;
         default:
